a36b0dbae9f286b7f7d2d978965889e9f31ea909
Fernando Robla <RoblaFx@gmail.com>
2025-05-01 17:17:25 +0200

    V0.6.3 - Footer componente

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { CategoriasComponent } from './categoria/categoria.component';
 import { HeroBuscadorComponent } from './hero-buscador/hero-buscador.component';
 import { NavbarBusquedaComponent } from './navbar-busqueda/navbar-busqueda.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NavbarBusquedaComponent } from './navbar-busqueda/navbar-busqueda.compo
     LoginComponent,
     CategoriasComponent,
     HeroBuscadorComponent,
-    NavbarBusquedaComponent
+    NavbarBusquedaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
